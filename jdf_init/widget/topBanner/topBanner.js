1634c62b29b725158269062362c85ad792139a5d
redliyang <lixusheng_php@qq.com>
2017-11-27 16:58:52 +0800

    jdf

---
+++ jdf_init/widget/topBanner/topBanner.js
@@ -0,0 +1,5 @@
+var topBanner = document.querySelector("#J_event");
+var xx = document.querySelector("#J_event_close");
+xx.addEventListener('click', function () {
+    topBanner.style.display = 'none';
+});
