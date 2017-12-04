df2e9013cbfed8dc9fd56e6b2b11af3c368f90b2
redliyang <lixusheng_php@qq.com>
2017-12-04 17:45:17 +0800

    1

--- ipplus360/jdf_init/widget/swiper/swiper.js
+++
@@ -1,5 +0,0 @@
-var topBanner = document.querySelector("#J_event");
-var xx = document.querySelector("#J_event_close");
-xx.addEventListener('click', function () {
-    topBanner.style.display = 'none';
-});
