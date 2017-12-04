2d7c887ea7bee8dd5f36ef2542b1cd4f23ae9374
redliyang <lixusheng_php@qq.com>
2017-12-04 14:54:01 +0800

    1

--- src/js/jd/index.js
+++ src/js/jd/index.js
@@ -1,8 +1,4 @@
-var topBanner = document.querySelector("#J_event");
-var xx = document.querySelector("#J_event_close");
-xx.addEventListener('click',function () {
-	topBanner.style.display = 'none';
-});
+
 
 
 function ajaxGetPlaceholder(callback){
